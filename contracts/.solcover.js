5ec4cd9d10f4c689a28b1508b4668b1ba3508e07
Akshay Meher <akshay_111meher@hotmail.com>
2023-11-29 13:14:08 +0400

    Update .solcover.js

--- contracts/.solcover.js
+++ contracts/.solcover.js
@@ -1,3 +1,9 @@
 module.exports = {
   configureYulOptimizer: true,
+  skipFiles: [
+    "../contracts/verifiers/plonk_vk.sol",
+    "../contracts/verifiers/transfer_verifier.sol",
+    "../contracts/verifiers/verifier_xor2.sol",
+    "../contracts/verifiers/zkb_verifier.sol",
+  ],
 };
