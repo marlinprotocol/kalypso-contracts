3d1c32935105da56573c40f6bcc7f61d76717789
Akshay Meher <akshay_111meher@hotmail.com>
2023-12-21 11:07:01 +0400

    Add Attestation Verifier

--- contracts/interfaces/IAttestationVerifier.sol
+++ contracts/interfaces/IAttestationVerifier.sol
@@ -1,9 +1,31 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 interface IAttestationVerifier {
-    function verify(bytes memory data) external returns (bool);
+    function verify(
+        bytes memory attestation,
+        address sourceEnclaveKey,
+        address enclaveKey,
+        bytes memory PCR0,
+        bytes memory PCR1,
+        bytes memory PCR2,
+        uint256 enclaveCPUs,
+        uint256 enclaveMemory
+    ) external view returns (bool);
 
-    function safeVerify(bytes memory data) external;
+    function safeVerify(
+        bytes memory attestation,
+        address sourceEnclaveKey,
+        address enclaveKey,
+        bytes memory PCR0,
+        bytes memory PCR1,
+        bytes memory PCR2,
+        uint256 enclaveCPUs,
+        uint256 enclaveMemory
+    ) external view;
+
+    function verify(bytes memory data) external view returns (bool);
+
+    function safeVerify(bytes memory data) external view;
 }
--- contracts/mock/MockAttestationVerifier.sol
+++ contracts/mock/MockAttestationVerifier.sol
@@ -5,9 +5,33 @@ pragma solidity ^0.8.9;
 import "../interfaces/IAttestationVerifier.sol";
 
 contract MockAttestationVerifier is IAttestationVerifier {
+    function verify(
+        bytes memory attestation,
+        address sourceEnclaveKey,
+        address enclaveKey,
+        bytes memory PCR0,
+        bytes memory PCR1,
+        bytes memory PCR2,
+        uint256 enclaveCPUs,
+        uint256 enclaveMemory
+    ) public pure override returns (bool) {
+        return true;
+    }
+
+    function safeVerify(
+        bytes memory attestation,
+        address sourceEnclaveKey,
+        address enclaveKey,
+        bytes memory PCR0,
+        bytes memory PCR1,
+        bytes memory PCR2,
+        uint256 enclaveCPUs,
+        uint256 enclaveMemory
+    ) public pure override {}
+
     function verify(bytes memory) public pure override returns (bool) {
         return true;
     }
 
-    function safeVerify(bytes memory) public override {}
+    function safeVerify(bytes memory data) public pure override {}
 }
