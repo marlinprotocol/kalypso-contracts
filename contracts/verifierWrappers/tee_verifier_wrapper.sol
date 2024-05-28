070edbe8243adb2f9448abfe66ac408946c06a73
Akshay Meher <akshay_111meher@hotmail.com>
2024-05-28 15:34:45 +0400

    Use AttestationAuther instead of EntityKeyRegistry in tee_verifier

--- contracts/verifierWrappers/tee_verifier_wrapper.sol
+++ contracts/verifierWrappers/tee_verifier_wrapper.sol
@@ -2,40 +2,45 @@
 
 pragma solidity ^0.8.0;
 
-import "../interfaces/SetPmp.sol";
-
+import "../periphery/AttestationAuther.sol";
+import "../lib/Helper.sol";
 import "../interfaces/IVerifier.sol";
-import "../EntityKeyRegistry.sol";
 
-contract tee_verifier_wrapper is SetPmp, IVerifier {
-    EntityKeyRegistry public immutable ENTITY_KEY_REGISTRY;
+contract tee_verifier_wrapper_factory {
+    function create_tee_verifier_wrapper(IAttestationVerifier _av, bytes[] calldata _proverPcrs) public returns (tee_verifier_wrapper) {
+        return new tee_verifier_wrapper(_av, _proverPcrs);
+    }
+}
+
+contract tee_verifier_wrapper is AttestationAuther, IVerifier {
     bytes public override sampleInput;
     bytes public override sampleProof;
 
     using HELPER for bytes32;
+    using HELPER for bytes;
 
-    constructor(EntityKeyRegistry _entityRegistry) {
-        ENTITY_KEY_REGISTRY = _entityRegistry;
-        require(checkSampleInputsAndProof(), "Can't be deployed");
+    bytes32 constant FAMILY_ID = keccak256("FAMILY_ID");
+
+    constructor(IAttestationVerifier _av, bytes[] memory _proverPcrs) AttestationAuther(_av, HELPER.ACCEPTABLE_ATTESTATION_DELAY) {
+        for (uint256 index = 0; index < _proverPcrs.length; index++) {
+            (bytes memory PCR0, bytes memory PCR1, bytes memory PCR2) = abi.decode(_proverPcrs[index], (bytes, bytes, bytes));
+
+            bytes32 imageId = PCR0.GET_IMAGE_ID_FROM_PCRS(PCR1, PCR2);
+            if (!imageId.IS_ENCLAVE()) {
+                revert Error.MustBeAnEnclave(imageId);
+            }
+
+            (bytes32 inferredImageId, ) = _whitelistEnclaveImage(EnclaveImage(PCR0, PCR1, PCR2));
+
+            if (inferredImageId != imageId) {
+                revert Error.InferredImageIdIsDifferent();
+            }
+            _addEnclaveImageToFamily(imageId, FAMILY_ID);
+        }
     }
 
-    function createRequest(
-        ProofMarketplace.Ask calldata ask,
-        ProofMarketplace.SecretType secretType,
-        bytes calldata secret_inputs,
-        bytes calldata acl
-    ) public {
-        ProofMarketplace.Ask memory newAsk = ProofMarketplace.Ask(
-            ask.marketId,
-            ask.reward,
-            ask.expiry,
-            ask.timeTakenForProofGeneration,
-            ask.deadline,
-            ask.refundAddress,
-            encodeInputs(verifyAndDecodeInputs(ask.proverData))
-        );
-
-        proofMarketplace.createAsk(newAsk, secretType, abi.encode(secret_inputs), abi.encode(acl));
+    function verifyKey(bytes calldata attestation_data) external {
+        _verifyKeyInternal(attestation_data);
     }
 
     function verifyAndDecodeInputs(bytes calldata inputs) internal pure returns (string[] memory) {
@@ -53,27 +58,20 @@ contract tee_verifier_wrapper is SetPmp, IVerifier {
     }
 
     function verify(bytes memory encodedData) public view override returns (bool) {
-        uint256 askId;
-        bytes memory proofSignature;
-        bytes32 familyId;
+        (bytes memory encodedInputs, bytes memory encodedProof, bytes memory proofSignature) = abi.decode(
+            encodedData,
+            (bytes, bytes, bytes)
+        );
 
-        (bytes memory encodedAskId, bytes memory encodedInputs, bytes memory encodedProofSignature, bytes memory encodedFamilyId) = abi
-            .decode(encodedData, (bytes, bytes, bytes, bytes));
-
-        (askId) = abi.decode(encodedAskId, (uint256));
-        (proofSignature) = abi.decode(encodedProofSignature, (bytes));
-        (familyId) = abi.decode(encodedFamilyId, (bytes32));
-
-        return verifyProofForTeeVerifier(askId, encodedInputs, proofSignature, familyId);
+        return verifyProofForTeeVerifier(encodedInputs, encodedProof, proofSignature);
     }
 
     function verifyProofForTeeVerifier(
-        uint256 askId,
         bytes memory proverData,
-        bytes memory proofSignature,
-        bytes32 familyId
+        bytes memory proofData,
+        bytes memory proofSignature
     ) public view returns (bool) {
-        bytes32 messageHash = keccak256(abi.encode(askId, proverData));
+        bytes32 messageHash = keccak256(abi.encode(proverData, proofData));
 
         bytes32 ethSignedMessageHash = messageHash.GET_ETH_SIGNED_HASHED_MESSAGE();
 
@@ -82,7 +80,7 @@ contract tee_verifier_wrapper is SetPmp, IVerifier {
             revert Error.InvalidEnclaveSignature(signer);
         }
 
-        ENTITY_KEY_REGISTRY.allowOnlyVerifiedFamily(familyId, signer);
+        _allowOnlyVerifiedFamily(signer, FAMILY_ID);
         return true;
     }
 
@@ -101,5 +99,19 @@ contract tee_verifier_wrapper is SetPmp, IVerifier {
 
     function encodeInputAndProofForVerification(string[] memory inputs, string memory proof) public pure returns (bytes memory) {
         return abi.encode(encodeInputs(inputs), encodeProof(proof));
+    }
+
+    function _verifyKeyInternal(bytes calldata data) internal {
+        (
+            bytes memory attestation,
+            bytes memory enclaveKey,
+            bytes memory PCR0,
+            bytes memory PCR1,
+            bytes memory PCR2,
+            uint256 timestamp
+        ) = abi.decode(data, (bytes, bytes, bytes, bytes, bytes, uint256));
+
+        // compute image id in proper way
+        _verifyEnclaveKey(attestation, IAttestationVerifier.Attestation(enclaveKey, PCR0, PCR1, PCR2, timestamp));
     }
 }
