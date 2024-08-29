c8928f158d506a09af75c53cbed5f3fd614bc864
Frenchkebab <tubowangin@gmail.com>
2024-08-29 22:10:23 +0900

    add check logic for submitSnapshot

--- contracts/staking/l2_contracts/SymbioticStaking.sol
+++ contracts/staking/l2_contracts/SymbioticStaking.sol
@@ -5,78 +5,56 @@ contract SymbioticStaking {
     // TODO: address Operator => address token => CheckPoints.Trace256 stakeAmount (Question: operators' stake amount is consolidated within same vault?)
 
     // TODO: set SD
+    uint256 SD;
 
     // TODO: set TC
 
     // TODO: lastCapturedTimestamp
+    uint256 lastCapturedTimestamp;
 
     //? How to manage Vault lists?
+
+    struct SnapshotInfo {
+        uint256 count;
+        uint256 length;
+    }
+
+    mapping(uint256 captureTimestamp => mapping(address account => SnapshotInfo snapshot)) snapshotInfo;
     
     // Transmitter submits staking data snapshot
     // This should update StakingManger's state
     function submitSnapshot(
-        uint256 txIndex,
-        uint256 noOfTxs,
+        uint256 index,
+        uint256 length, // number of total transactions
         uint256 captureTimestamp,
         bytes memory stakeData,
         bytes memory signature
     ) external {
-        // TODO: check) captureTimestamp >= lastCaptureTimestamp + SD
-        
-        // TODO: Check) noOfTxs txIndex
+        require(block.timestamp >= lastCapturedTimestamp + SD, "Cooldown period not passed");
 
-        // TODO: Check) noOfTxs should be consistent across all the partial snapshots
+        require(length > 0, "Invalid length");
         
-        // TODO: Data transmitter should get TC% of the rewards
+        require(index < length, "Invalid index");
 
+        require(snapshotInfo[captureTimestamp][msg.sender].count > 0, "Snapshot fully submitted already");
+        snapshotInfo[captureTimestamp][msg.sender].count--;
+
+        require(snapshotInfo[captureTimestamp][msg.sender].length == length, "Invalid length");
+
+        // TODO: Verify the signature
         // TODO: "signature" should be from the enclave key that is verified against the PCR values of the bridge enclave image
 
-        // TODO: stakeData should be of the correct format which has key value pairs of operators and stakeDelta
+        // TODO: Data transmitter should get TC% of the rewards
+
+        // TODO: stakeData should be of the correct format which has key value pairs of operators and stakeDelta (?)
 
         // TODO: "TC" should reflect incentivization mechanism based on "captureTimestamp - (lastCaptureTimestamp + SD)"
 
         // TODO: Should update the latest complete snapshot information once the last chunk of staking snapshot is received (Updates TC based on the delay)
     }
 
+
     /*======================================== Getters ========================================*/
 
-    function getLatestStakingAmount() external view returns (address[] memory tokens, uint256[] memory amounts) {
-        // TODO
-    }
 
-    function getStakingAmountAt(uint256 timestamp) external view returns (uint256 amount) {
-        // TODO
-    }
-    
-    function getLatestStakingAmount(address token) external view returns (uint256 amount) {
-        // TODO
-    }
-
-    function getStakingAmountAt(address token, uint256 timestamp) external view returns (uint256 amount) {
-        // TODO
-    }
-
-    // returns latest stake amount of an Operator for all tokens
-    function getLatestOperatorStakingAmount(address operator) external view returns (address[] memory tokens, uint256[] memory amounts) {
-        // TODO
-    }
-    
-    // returns latest stake amount of an Operator for a specific token
-    function getLatestOperatorStakingAmount(address operator, address token) external view returns (uint256 amount) {
-        // TODO
-    }
-
-    // returns stake amounts of an Operator for all tokens at a specific timestamp
-    function getOperatorStakingAmountAt(address operator, uint256 timestamp) external view returns (address[] memory tokens, uint256[] memory amounts ) {
-        // TODO
-    }
-
-    // returns stake amount of an Operator for a specific token at a specific timestamp
-    function getOperatorStakingAmountAt(address operator, address token, uint256 timestamp) external view returns (uint256 amount) {
-        // TODO
-    }
-    
-    function slashSymbioticVault(address operator, address vault, uint256 captureTimestamp, uint256 amount, address rewardAddress) external {
-        // TODO only slashingManager
-    }
 }
