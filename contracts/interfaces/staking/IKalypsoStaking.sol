40e0b0154f1650163f4c6d0a4753d042fec869b3
Frenchkebab <tubowangin@gmail.com>
2024-09-02 00:52:14 +0900

    add logic for jobCreation and stake lock

--- contracts/interfaces/staking/IKalypsoStaking.sol
+++ contracts/interfaces/staking/IKalypsoStaking.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-interface IKalypsoStaking {
+interface IKalypsoStakingPool {
     // function stakeOf(address _operator, address _token) external view returns (uint256);
+
+    function isSupportedToken(address _token) external view returns (bool);
+
+    function getStakeAmount(address _operator, address _token) external view returns (uint256);
+
+    function getStakeAmountList(address _operator) external view returns (address[] memory _operators, uint256[] memory _amounts);
 }
--- contracts/staking/l2_contracts/StakingManager.sol
+++ contracts/staking/l2_contracts/StakingManager.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
+
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import {ERC165Upgradeable} from "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
@@ -17,9 +18,34 @@ contract StakingManager is
     AccessControlUpgradeable,
     UUPSUpgradeable,
     ReentrancyGuardUpgradeable,
-    INativeStaking {
+    INativeStaking
+{
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using SafeERC20 for IERC20;
+
     // TODO: Staking Pool Set
-    // TODO: Staking Pool flag   
+    EnumerableSet.AddressSet private stakingPoolSet;
+
+    // TODO: Staking Pool flag
+    // mapping(address pool => bool isEnabled) private stakingPoolStatus;
+    // mapping(address pool => uint256 weight) private stakingPoolWeight;
+
+    mapping(address pool => PoolConfig config) private poolConfig;
+    mapping(uint256 jobId => mapping(address pool => LockInfo lockInfo)) private lockInfo;
+
+    uint256 unlockEpoch;
+
+    struct PoolConfig {
+        uint256 weight;
+        uint256 minStake;
+        bool isEnabled;
+    }
+
+    // operator, lockToken, lockAmount should be stored in JobManager
+    struct LockInfo {
+        uint256 lockAmount; // locked amount for the pool
+        uint256 unlockTimestamp;
+    }
 
     // TODO: integration with Slasher
 
@@ -34,29 +60,67 @@ contract StakingManager is
 
     // TODO: Self stake is given to the slasher while the rest of the stakers is burnt when slashed
     // TODO: check necessary params
-    function slashJob(address _jobId, address _vault, uint256 _captureTimestamp, uint256 _amount, address _rewardAddress) external {
+    function slashJob(
+        address _jobId,
+        address _vault,
+        uint256 _captureTimestamp,
+        uint256 _amount,
+        address _rewardAddress
+    ) external {
         // TODO: only slashingManager
     }
 
     // create job and lock stakes (operator self stake, some portion of native stake and symbiotic stake)
     // locked stake will be unlocked after an epoch if no slas result is submitted
-    function onJobCreation(address _jobId, address _operator, uint256 _amount) external {
+
+    // note: data related to the job should be stored in JobManager (e.g. operator, lockToken, lockAmount, proofDeadline)
+    function onJobCreation(address _jobId, address _operator, address token, uint256 _lockAmount) external {
         // TODO: only jobManager
+
+        // TODO: lock operator selfstake
+
+        address[] memory enabledPoolList = getEnabledPoolList();
+
+        uint256 len = enabledPoolList.length;
+        for(uint256 i = 0; i < len; i++) {
+            pool = enabledPoolList[i];
+
+            // skip if the token is not supported by the pool
+            if(!IKalypsoStakingPool(pool).isSupportedToken(token)) continue;
+            
+            uint256 poolStake = getPoolStake(pool, _operator, token);
+            uint256 minStake = poolConfig[pool].minStake;
+
+            // skip if the pool stake is less than the minStake
+            if(poolStake >= minStake) {
+                // lock the stake
+                uint256 lockAmount = _calcLockAmount(poolStake, poolConfig[pool].weight); // TODO: need to check formula for calculation
+                _lockStake(pool, lockAmount);
+            }
+        }
+    }
+
+    function _lockStake(address _jobId, uint256 amount) internal {
+        lockInfo[_jobId].lockAmount += amount;
+        lockInfo[_jobId].unlockTimestamp = block.timestamp + unlockEpoch;
+    }
+
+    function _unlockStake(address _jobId) internal {
+        lockInfo[_jobId].lockAmount = 0;
+        lockInfo[_jobId].unlockTimestamp = 0;
     }
 
     // called when job is completed to unlock the locked stakes
     function onJobCompletion(address _jobId) external {
         // TODO: only jobManager
-    }
 
-    // called when Staked/Unstaked in the Staking Pool (Native Staking, Symbiotic Staking)
-    function updateStake(address operator, address token, uint256 amount) external {
-        // TODO: only Staking Pool
+        // TODO: unlock the locked stakes
     }
 
     // when certain period has passed after the lock and no slash result is submitted, this can be unlocked
-    function unlockStake(address _jobId) external { }
-
+    function unlockStake(address _jobId) external {
+        
+    }
 
     /*======================================== Getters ========================================*/
 
@@ -64,29 +128,28 @@ contract StakingManager is
     // this only tells if the deadline for proof submission has passed
     // so even when this function returns true and transaction submitted to L1 can be reverted
     // when someone already has submitted the proof
-    function isSlashable(address _jobId) external view returns(bool) {
-        // TODO
+    function isSlashable(address _jobId) external view returns (bool) {
+        // TODO: check if the proof was submitted before the deadline
     }
 
-    // for all operators
-    function getLatestStakeInfo() public {
-        // TODO
+    /*======================================== Getter for Staking ========================================*/
+
+    function _calcLockAmount(uint256 amount, uint256 weight) internal pure returns (uint256) {
+        return (amount * weight) / 10000; // TODO: need to check formula for calculation (probably be the share)
     }
 
-    function getLatestStakeInfoAt(uint256 timestamp) public {
-        // TODO
+
+    function getEnabledPoolList() public view returns (address[] memory enabledPoolList) {
+        uint256 len = stakingPoolSet.length;
+        for(uint256 i = 0; i < len; i++) {
+            pool = stakingPoolSet.at(i);
+            
+            if(poolConfig[pool].isEnabled) {
+                enabledPoolList.push(pool);
+            }
+        }
     }
 
-    // for a specific operator
-    function getLatestStakeInfo(address operator) public {
-        // TODO
-    }
-
-    function getStakeInfoAt(address operator, address token, uint256 timestamp) public returns(uint256) {
-        // TODO
-    }
-
-    // TODO: function that consolidates the staking information from both Native Staking and Symbiotic Staking and returns the total stake amount
 
     /*======================================== Admin ========================================*/
 
@@ -97,7 +160,7 @@ contract StakingManager is
 
     function removeStakingPool(address _stakingPool) external {
         // TODO: onlyAdmin
-    } 
+    }
 
     function setStakingPoolStatus(address _stakingPool, bool _status) external {
         // TODO: onlyAdmin
@@ -117,8 +180,12 @@ contract StakingManager is
         // TODO
     }
 
+    function setUnlockEpoch(uint256 _unlockEpoch) external {
+        // TODO: check if the unlockEpoch is longer than the proofDeadline
+    }
+
     /*======================================== Override ========================================*/
-    
+
     function supportsInterface(bytes4 interfaceId)
         public
         view
