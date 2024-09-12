58908454c4ddf92851a69a6dbd2b57adce9f2783
Frenchkebab <tubowangin@gmail.com>
2024-09-12 13:38:36 +0900

    add upgradeable contracts inheritance

--- contracts/staking/l2_contracts/SymbioticStakingReward.sol
+++ contracts/staking/l2_contracts/SymbioticStakingReward.sol
@@ -2,30 +2,72 @@
 
 pragma solidity ^0.8.26;
 
+import {ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import {ERC165Upgradeable} from "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
 /* 
     Unlike common staking contracts, this contract is interacted each time snapshot is submitted to Symbiotic Staking,
     which means the state of each vault address will be updated whenvever snapshot is submitted.
 */
 
-contract SymbioticStakingReward {
+contract SymbioticStakingReward is
+    ContextUpgradeable,
+    ERC165Upgradeable,
+    AccessControlUpgradeable,
+    ReentrancyGuardUpgradeable,
+    PausableUpgradeable,
+    UUPSUpgradeable
+{
     //? what should be done when stake is locked?
     // -> just update totalStakeAmount and rewardPerToken?
     // -> does this even affect anything?
 
     // TODO: (mapping) captureTimestamp => token => vault => amount
-    
+
     // TODO: (mapping) captureTimestamp => token => totalStakeAmount
-    
+
     // TODO: (mapping) token => rewardPerToken
-    
+
     // TODO: (array) confirmed timestamp
 
     // TODO: (function) function that updates confirmed timestamp
     // this should be called by StakingManager contract when partial txs are completed
-    
+
     // TODO: (function) updates reward per token for each submission
-    
+
     // TODO: admin
 
     // TODO: claim
+
+    //-------------------------------- Init start --------------------------------//
+
+    function initialize(address _admin) public initializer {
+        __Context_init_unchained();
+        __ERC165_init_unchained();
+        __AccessControl_init_unchained();
+        __UUPSUpgradeable_init_unchained();
+        __ReentrancyGuard_init_unchained();
+        __ReentrancyGuard_init_unchained();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+    }
+
+    //-------------------------------- Init end --------------------------------//
+
+
+    //-------------------------------- Overrides start --------------------------------//
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC165Upgradeable, AccessControlUpgradeable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _authorizeUpgrade(address /*account*/) internal view override onlyRole(DEFAULT_ADMIN_ROLE) {}
+
+    //-------------------------------- Overrides end --------------------------------//
 }
