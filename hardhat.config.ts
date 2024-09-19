10d3ab6940c65ad6e7d5adc30cf537d55dba6000
Frenchkebab <tubowangin@gmail.com>
2024-09-19 13:53:20 +0800

    hardhat config for compile

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,15 @@
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@openzeppelin/hardhat-upgrades";
-import "@nomicfoundation/hardhat-chai-matchers";
+import '@nomicfoundation/hardhat-toolbox';
+import '@openzeppelin/hardhat-upgrades';
+import '@nomicfoundation/hardhat-chai-matchers';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
 
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-
-import { config as dotenvConfig } from "dotenv";
-
-import BigNumber from "bignumber.js";
+import BigNumber from 'bignumber.js';
+import { config as dotenvConfig } from 'dotenv';
+import {
+  HardhatUserConfig,
+  task,
+} from 'hardhat/config';
 
 dotenvConfig();
 
@@ -24,6 +25,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
+      {
+        version: "0.8.26",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
       {
         version: "0.8.24",
         settings: {
@@ -78,67 +89,67 @@ const config: HardhatUserConfig = {
     hardhat: {
       blockGasLimit: 500000000000,
     },
-    sepolia: {
-      url: `${process.env.SEPOLIA_RPC_URL}`,
-      // NOTE: don't change the order of elements in the array, add new elements at the last.
-      accounts: [
-        `${process.env.SEPOLIA_ADMIN}`,
-        `${process.env.SEPOLIA_TOKEN_HOLDER}`,
-        `${process.env.SEPOLIA_TREASURY}`,
-        `${process.env.SEPOLIA_MARKET_CREATOR}`,
-        `${process.env.SEPOLIA_GENERATOR}`,
-        `${process.env.SEPOLIA_MATCHING_ENGINE}`,
-        `${process.env.SEPOLIA_PROOF_REQUESTOR}`,
-      ],
-    },
-    arbSepolia: {
-      url: `${process.env.ARB_SEPOLIA_RPC_URL}`,
-      accounts: [
-        `${process.env.SEPOLIA_ADMIN}`,
-        `${process.env.SEPOLIA_TOKEN_HOLDER}`,
-        `${process.env.SEPOLIA_TREASURY}`,
-        `${process.env.SEPOLIA_MARKET_CREATOR}`,
-        `${process.env.SEPOLIA_GENERATOR}`,
-        `${process.env.SEPOLIA_MATCHING_ENGINE}`,
-        `${process.env.SEPOLIA_PROOF_REQUESTOR}`,
-      ],
-    },
-    nova: {
-      url: `${process.env.NOVA_RPC_URL}`,
-      accounts: [
-        `${process.env.NOVA_ADMIN}`,
-        `${process.env.NOVA_TOKEN_HOLDER}`,
-        `${process.env.NOVA_TREASURY}`,
-        `${process.env.NOVA_MARKET_CREATOR}`,
-        `${process.env.NOVA_GENERATOR}`,
-        `${process.env.NOVA_MATCHING_ENGINE}`,
-        `${process.env.NOVA_PROOF_REQUESTOR}`,
-      ],
-    },
-    zksync: {
-      url: `${process.env.ZKSYNC_URL}`,
-      accounts: [
-        `${process.env.ZKSYNC_ADMIN}`,
-        `${process.env.ZKSYNC_TOKEN_HOLDER}`,
-        `${process.env.ZKSYNC_TREASURY}`,
-        `${process.env.ZKSYNC_MARKET_CREATOR}`,
-        `${process.env.ZKSYNC_GENERATOR}`,
-        `${process.env.ZKSYNC_MATCHING_ENGINE}`,
-        `${process.env.ZKSYNC_PROOF_REQUESTOR}`,
-      ],
-    },
-    amoy: {
-      url: `${process.env.AMOY_RPC}`,
-      accounts: [
-        `${process.env.AMOY_ADMIN}`,
-        `${process.env.AMOY_TOKEN_HOLDER}`,
-        `${process.env.AMOY_TREASURY}`,
-        `${process.env.AMOY_MARKET_CREATOR}`,
-        `${process.env.AMOY_GENERATOR}`,
-        `${process.env.AMOY_MATCHING_ENGINE}`,
-        `${process.env.AMOY_PROOF_REQUESTOR}`,
-      ],
-    },
+    // sepolia: {
+    //   url: `${process.env.SEPOLIA_RPC_URL}`,
+    //   // NOTE: don't change the order of elements in the array, add new elements at the last.
+    //   accounts: [
+    //     `${process.env.SEPOLIA_ADMIN}`,
+    //     `${process.env.SEPOLIA_TOKEN_HOLDER}`,
+    //     `${process.env.SEPOLIA_TREASURY}`,
+    //     `${process.env.SEPOLIA_MARKET_CREATOR}`,
+    //     `${process.env.SEPOLIA_GENERATOR}`,
+    //     `${process.env.SEPOLIA_MATCHING_ENGINE}`,
+    //     `${process.env.SEPOLIA_PROOF_REQUESTOR}`,
+    //   ],
+    // },
+    // arbSepolia: {
+    //   url: `${process.env.ARB_SEPOLIA_RPC_URL}`,
+    //   accounts: [
+    //     `${process.env.SEPOLIA_ADMIN}`,
+    //     `${process.env.SEPOLIA_TOKEN_HOLDER}`,
+    //     `${process.env.SEPOLIA_TREASURY}`,
+    //     `${process.env.SEPOLIA_MARKET_CREATOR}`,
+    //     `${process.env.SEPOLIA_GENERATOR}`,
+    //     `${process.env.SEPOLIA_MATCHING_ENGINE}`,
+    //     `${process.env.SEPOLIA_PROOF_REQUESTOR}`,
+    //   ],
+    // },
+    // nova: {
+    //   url: `${process.env.NOVA_RPC_URL}`,
+    //   accounts: [
+    //     `${process.env.NOVA_ADMIN}`,
+    //     `${process.env.NOVA_TOKEN_HOLDER}`,
+    //     `${process.env.NOVA_TREASURY}`,
+    //     `${process.env.NOVA_MARKET_CREATOR}`,
+    //     `${process.env.NOVA_GENERATOR}`,
+    //     `${process.env.NOVA_MATCHING_ENGINE}`,
+    //     `${process.env.NOVA_PROOF_REQUESTOR}`,
+    //   ],
+    // },
+    // zksync: {
+    //   url: `${process.env.ZKSYNC_URL}`,
+    //   accounts: [
+    //     `${process.env.ZKSYNC_ADMIN}`,
+    //     `${process.env.ZKSYNC_TOKEN_HOLDER}`,
+    //     `${process.env.ZKSYNC_TREASURY}`,
+    //     `${process.env.ZKSYNC_MARKET_CREATOR}`,
+    //     `${process.env.ZKSYNC_GENERATOR}`,
+    //     `${process.env.ZKSYNC_MATCHING_ENGINE}`,
+    //     `${process.env.ZKSYNC_PROOF_REQUESTOR}`,
+    //   ],
+    // },
+    // amoy: {
+    //   url: `${process.env.AMOY_RPC}`,
+    //   accounts: [
+    //     `${process.env.AMOY_ADMIN}`,
+    //     `${process.env.AMOY_TOKEN_HOLDER}`,
+    //     `${process.env.AMOY_TREASURY}`,
+    //     `${process.env.AMOY_MARKET_CREATOR}`,
+    //     `${process.env.AMOY_GENERATOR}`,
+    //     `${process.env.AMOY_MATCHING_ENGINE}`,
+    //     `${process.env.AMOY_PROOF_REQUESTOR}`,
+    //   ],
+    // },
   },
 };
 
