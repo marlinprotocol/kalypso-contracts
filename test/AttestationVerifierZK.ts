34e6e735c918ffc835c543b0a183f2929d1f1bc2
Kalpita Mandal <kalpita103@gmail.com>
2024-10-23 11:08:11 +0530

    Adding code to verify risc0 attestation verifier contract

--- test/AttestationVerifierZK.ts
+++ test/AttestationVerifierZK.ts
@@ -1,6 +1,6 @@
 import { ethers, upgrades } from "hardhat";
 import { expect } from "chai";
-import { Signer, ZeroAddress } from "ethers";
+import { BytesLike, Signer, ZeroAddress } from "ethers";
 import * as receipt from "../helpers/sample/risc0/receipt.json";
 // import * as attestation from "../helpers/sample/risc0/attestation.json";
 import * as attestation from "../helpers/sample/risc0/final.json";
@@ -9,7 +9,8 @@ import {
     RiscZeroGroth16Verifier,
     RiscZeroGroth16Verifier__factory,
     AttestationVerifierZK,
-    AttestationVerifierZK__factory
+    AttestationVerifierZK__factory,
+    IAttestationVerifier
 } from "../typechain-types";
 
 describe("Attestation verifier for RISC0, testing", () => {
@@ -24,13 +25,27 @@ describe("Attestation verifier for RISC0, testing", () => {
         admin = signers[0];
 
         //arbSepolia
-        // https://dev.risczero.com/api/blockchain-integration/contracts/verifier#arbitrum-sepolia-421614
-        // control_id 0x05a022e1db38457fb510bc347b30eb8f8cf3eda95587653d0eac19e1f10d164e
-        // control_root_0 0x3f05edb31fb4615345ac63d411cf6d8b
-        // control_root_1 0x2e853b01450e34db0fa0de1b4917b8eb
-        riscZeroVerifier = RiscZeroGroth16Verifier__factory.connect('0x84b943E31e7fAe6072ce5F75eb4694C7D5F9b0cF');
+        // // https://dev.risczero.com/api/blockchain-integration/contracts/verifier#arbitrum-sepolia-421614
+        // // control_id 0x05a022e1db38457fb510bc347b30eb8f8cf3eda95587653d0eac19e1f10d164e
+        // // control_root_0 0x3f05edb31fb4615345ac63d411cf6d8b
+        // // control_root_1 0x2e853b01450e34db0fa0de1b4917b8eb
+        // // 0xbDB8F2Cc624625B80FCCaBEF04BC5420eF232dfB
+        // riscZeroVerifier = RiscZeroGroth16Verifier__factory.connect('0x84b943E31e7fAe6072ce5F75eb4694C7D5F9b0cF');
+        // const _attestattionVerifierZK = await new AttestationVerifierZK__factory(admin).deploy(
+        //     await riscZeroVerifier.getAddress()
+        // );
+        // attestationVerifierZK = AttestationVerifierZK__factory.connect(await _attestattionVerifierZK.getAddress(), admin);
+        // console.log("Contract address", await attestationVerifierZK.getAddress());
+        // expect(attestationVerifierZK.getAddress()).to.not.eq(ZeroAddress);
+
+        //Hardhat
+        riscZeroVerifier = await new RiscZeroGroth16Verifier__factory(admin).deploy(
+          "0x05a022e1db38457fb510bc347b30eb8f8cf3eda95587653d0eac19e1f10d164e",
+          "0x05a022e1db38457fb510bc347b30eb8f8cf3eda95587653d0eac19e1f10d164e"
+        );
+        const attestationVerifierZKContract = await ethers.getContractFactory("AttestationVerifierZK");
         const _attestattionVerifierZK = await new AttestationVerifierZK__factory(admin).deploy(
-            await riscZeroVerifier.getAddress()
+          await riscZeroVerifier.getAddress()
         );
         attestationVerifierZK = AttestationVerifierZK__factory.connect(await _attestattionVerifierZK.getAddress(), admin);
         console.log("Contract address", await attestationVerifierZK.getAddress());
@@ -62,7 +77,6 @@ describe("Attestation verifier for RISC0, testing", () => {
         // console.log("journal: ", journal);
 
         const encoded = abiCoder.encode(["string", "string", "string"], [seal, guest_id, journal]);
-        console.log("line62");
         const digest = ethers.keccak256(encoded);
         const signature = await admin.signMessage(ethers.getBytes(digest));
 
@@ -77,9 +91,15 @@ describe("Attestation verifier for RISC0, testing", () => {
         // let ts = abiCoder.encode(["uint256[8]"], [attestation.timestampInMilliseconds]);
         // let time_ts = abiCoder.decode(["uint256"], ts);
         // console.log("ts: ", time_ts);
-        let input_attest = JSON.stringify(attestation);
-
-        let checker = await attestationVerifierZK.getFunction("verify(bytes,(bytes,bytes,bytes,bytes,uint256))").staticCallResult(signature, attestation);
+        let input: IAttestationVerifier.AttestationStruct = {
+          enclavePubKey: attestation.enclavePubKey,
+          PCR0: attestation.PCR0,
+          PCR1: attestation.PCR1,
+          PCR2: attestation.PCR2,
+          timestampInMilliseconds: attestation.timestampInMilliseconds
+        };
+        
+        let checker = await attestationVerifierZK.getFunction("verify(bytes,(bytes,bytes,bytes,bytes,uint256))").call("attestationVerifierZK", signature, input);
         console.log("Checker: ", checker);
     });
 });
