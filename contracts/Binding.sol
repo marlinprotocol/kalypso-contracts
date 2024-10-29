04bee8241dcf17aea457f5db993e27233751b133
Akshay <akshay_111meher@hotmail.com>
2024-10-29 09:20:51 +0000

    Add Binding ref to generator only require bindings

---
+++ contracts/Binding.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "./EntityKeyRegistry.sol";
+import "./GeneratorRegistry.sol";
+import "./ProofMarketplace.sol";
+
+contract Binding {
+    
+}
